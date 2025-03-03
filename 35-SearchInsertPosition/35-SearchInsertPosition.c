2fec66c787514b972cc59c847953a97133e506cd
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-03-03 15:08:48 -0500

    File updated at 03/03/2025, 15:08:48

--- 35-SearchInsertPosition/35-SearchInsertPosition.c
+++ 35-SearchInsertPosition/35-SearchInsertPosition.c
@@ -1,18 +1,13 @@
 int searchInsert(int* nums, int numsSize, int target) {
+    int low = 0, high = numsSize - 1;
     
-    int low = 0, high = numsSize-1;
-    int ans = numsSize;
-
     while (low <= high) {
-        int mid = low + (high - low)/2;
-        if (nums[mid] >= target) {
-            ans = mid;
-            high = mid-1;
-        } else {
-            low = mid+1;
-        }
+        int mid = low + (high - low) / 2;
+        if (nums[mid] >= target)
+            high = mid - 1;
+        else
+            low = mid + 1;
     }
 
-    return ans;
-
+    return low;  // Instead of using 'ans'
 }
