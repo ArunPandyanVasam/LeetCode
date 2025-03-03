bccd4c0e12a94cd6a6d6bfcb2600679c26ba9646
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-03-02 19:25:35 -0500

    File updated at 02/03/2025, 19:25:35

--- 189-RotateArray/189-RotateArray.c
+++ 189-RotateArray/189-RotateArray.c
@@ -1,20 +1,31 @@
 void rotate(int* nums, int numsSize, int k) {
 
-    if (numsSize == 0 || k == 0) return;
-    k = k%numsSize;
-    if (k == 0) return;
-
-    int temp[k];
-    for (int i = numsSize-k; i < numsSize; i++) {
-        temp[i-numsSize+k] = nums[i];
+    if (numsSize <= 1 || k == 0) {
+        return;
     }
 
-    for (int i = numsSize-k-1; i>=0 ; i--) {
-        nums[i+k] = nums[i];
+    k = k % numsSize;
+    if (k == 0) {
+        return;
+    }
+
+    int* temp = (int*)malloc(k * sizeof(int));
+    if (temp == NULL) {
+        return;
+    }
+
+    for (int i = numsSize - k; i < numsSize; i++) {
+        temp[i - (numsSize - k)] = nums[i];
+    }
+
+    for (int i = numsSize - k - 1; i >= 0; i--) {
+        nums[i + k] = nums[i];
     }
 
     for (int i = 0; i < k; i++) {
         nums[i] = temp[i];
     }
+
+    free(temp);
 
 }
