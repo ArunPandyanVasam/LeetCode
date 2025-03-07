6efcc942751edb5824d4ae34643115b8cf114b55
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-03-07 18:20:48 -0500

    File updated at 07/03/2025, 18:20:48

--- 189-RotateArray/189-RotateArray.c
+++ 189-RotateArray/189-RotateArray.c
@@ -5,7 +5,7 @@ void rotateRight(int* nums, int low, int high);
 void rotate(int* nums, int numsSize, int k) {
 
     k = k % numsSize;
-    if (k == 0) return;
+    if (k == 0 || numsSize <= 1) return;
 
 
     rotateRight(nums, 0, numsSize-k-1);
