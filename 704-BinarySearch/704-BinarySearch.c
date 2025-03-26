e1d5c8886aa7accb5279c9fe5ca25c59904787d4
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-03-25 20:33:03 -0400

    File updated at 25/03/2025, 20:33:03

--- 704-BinarySearch/704-BinarySearch.c
+++ 704-BinarySearch/704-BinarySearch.c
@@ -1,20 +1,17 @@
+// Last updated: 25/03/2025, 20:33:03
 int search(int* nums, int numsSize, int target) {
-    
-    int low = 0, high = numsSize-1;
-    
+    int low = 0;
+    int high = numsSize-1;
+
     while (low <= high) {
         int mid = low+(high-low)/2;
-
         if (nums[mid] == target) {
             return mid;
-        } else if (target > nums[mid]) {
+        } else if (nums[mid] < target) {
             low = mid+1;
         } else {
             high = mid-1;
         }
-
     }
-
     return -1;
-
 }
