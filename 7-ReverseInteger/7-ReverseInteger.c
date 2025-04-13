7c9a8a41f285e6a1bbb984ae71adbec1ea0eb6e9
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-04-13 08:40:47 -0400

    Create file

---
+++ 7-ReverseInteger/7-ReverseInteger.c
@@ -0,0 +1,21 @@
+// Last updated: 13/04/2025, 08:40:47
+int reverse(int x) {
+    int reverseNumber = 0;
+
+    while (x != 0) {
+        int digit = x % 10;
+
+        // Check for overflow (positive side)
+        if (reverseNumber > INT_MAX / 10 || 
+           (reverseNumber == INT_MAX / 10 && digit > 7)) return 0;
+
+        // Check for underflow (negative side)
+        if (reverseNumber < INT_MIN / 10 || 
+           (reverseNumber == INT_MIN / 10 && digit < -8)) return 0;
+
+        reverseNumber = reverseNumber * 10 + digit;
+        x /= 10;
+    }
+
+    return reverseNumber;
+}
