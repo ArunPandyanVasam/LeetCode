3c9475cde0638cbd9e98d95e9ffbed2fe50b3aab
Arun Pandyan Vasam <128243193+ArunPandyanVasam@users.noreply.github.com>
2025-04-23 06:51:08 -0400

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.c
@@ -0,0 +1,22 @@
+// Last updated: 23/04/2025, 06:51:07
+bool isPalindrome(int x) {
+
+    if (x < 0) {
+        return false;
+    }
+
+    int reverseNumber = 0;
+    int n = x;
+
+    while (n > 0) {
+        int lastDigit = n%10;
+        if (reverseNumber > INT_MAX/10 || reverseNumber == INT_MAX/10 && lastDigit > 7) {
+            return false;
+        }
+        reverseNumber = (reverseNumber*10)+lastDigit;
+        n = n/10;
+    }
+
+    return x == reverseNumber;
+
+}
